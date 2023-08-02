891c0b77283a0f39f79cb185cb833273dbbb16eb
Remi Collin <sharon.code@proton.me>
2023-08-02 15:53:36 +0200

    add in containers/NewBill.js if to block file != jpeg, jpg, png

--- src/containers/Dashboard.js
+++ src/containers/Dashboard.js
@@ -133,6 +133,7 @@ export default class {
   handleShowTickets(e, bills, index) {
     if (this.counter === undefined || this.index !== index) this.counter = 0
     if (this.index === undefined || this.index !== index) this.index = index
+    console.log(this.counter)
     if (this.counter % 2 === 0) {
       bills.forEach(bill => {
         $(`#open-bill${bill.id}`).off();
@@ -140,7 +141,7 @@ export default class {
       $(`#arrow-icon${this.index}`).css({ transform: 'rotate(0deg)'})
       $(`#status-bills-container${this.index}`)
         .html(cards(filteredBills(bills, getStatus(this.index))))
-      this.counter=1
+      this.counter++
     } else {
       bills.forEach(bill => {
         $(`#open-bill${bill.id}`).off();
@@ -148,7 +149,7 @@ export default class {
       $(`#arrow-icon${this.index}`).css({ transform: 'rotate(90deg)'})
       $(`#status-bills-container${this.index}`)
         .html("")
-      this.counter=0
+      this.counter++
     }
 
     bills.forEach(bill => {
--- src/containers/NewBill.js
+++ src/containers/NewBill.js
@@ -15,6 +15,7 @@ export default class NewBill {
     this.billId = null
     new Logout({ document, localStorage, onNavigate })
   }
+  
   handleChangeFile = e => {
     e.preventDefault()
     const file = this.document.querySelector(`input[data-testid="file"]`).files[0]
@@ -40,8 +41,16 @@ export default class NewBill {
         this.fileName = fileName
       }).catch(error => console.error(error))
   }
+
   handleSubmit = e => {
     e.preventDefault()
+
+    const fileExtension = this.document.querySelector(`input[data-testid="file"]`).files[0].name.toLowerCase().match(/\.(jpg|png|jpeg)$/);
+    if(fileExtension === null) {
+      console.log("error")
+      return false
+    }
+
     console.log('e.target.querySelector(`input[data-testid="datepicker"]`).value', e.target.querySelector(`input[data-testid="datepicker"]`).value)
     const email = JSON.parse(localStorage.getItem("user")).email
     const bill = {
