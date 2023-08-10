3df0ddf14ce8b936f7abd3feae746af2ad832f8e
Remi Collin <sharon.code@proton.me>
2023-08-10 08:22:36 +0100

    try to test handleClickIconEye

--- src/__tests__/Bills.js
+++ src/__tests__/Bills.js
@@ -3,12 +3,12 @@
  */
 
 import '@testing-library/jest-dom'
-/* import { getByRole, getByTestId, getByLabelText } from '@testing-library/dom' */
+import {screen, waitFor, getByRole, getByTestId} from "@testing-library/dom"
 import userEvent from '@testing-library/user-event'
 
-import {screen, waitFor, getByRole} from "@testing-library/dom"
-import BillsUI from "../views/BillsUI.js"
-import { bills } from "../fixtures/bills.js"
+import BillsUI from "../views/BillsUI.js";
+import { bills } from "../fixtures/bills.js";
+import  billsPage  from "../containers/Bills.js";
 import { ROUTES_PATH} from "../constants/routes.js";
 import {localStorageMock} from "../__mocks__/localStorage.js";
 
@@ -33,6 +33,7 @@ describe("Given I am connected as an employee", () => {
       expect(windowIcon.classList.contains('active-icon')).toBe(true);
 
     })
+
     test("Then bills should be ordered from earliest to latest", () => {
       document.body.innerHTML = BillsUI({ data: bills })
       const dates = screen.getAllByText(/^(19|20)\d\d[- /.](0[1-9]|1[012])[- /.](0[1-9]|[12][0-9]|3[01])$/i).map(a => a.innerHTML)
@@ -41,8 +42,21 @@ describe("Given I am connected as an employee", () => {
       expect(dates).toEqual(datesSorted)
     })
 
-    test("Then it should show the justificatif when we click on icon-eye",  ()=>{
-      
+    test("Then it should show the justificatif when we click on icon-eye", async ()=>{
+      Object.defineProperty(window, 'localStorage', { value: localStorageMock })
+      window.localStorage.setItem('user', JSON.stringify({
+        type: 'Employee'
+      }))
+      const root = document.createElement("div")
+      root.setAttribute("id", "root")
+      document.body.append(root)
+      router()
+      window.onNavigate(ROUTES_PATH.Bills)
+      await waitFor(() => screen.getAllByTestId("icon-eye")[2])
+      userEvent.click(screen.getAllByTestId("icon-eye")[2])
+      await waitFor(()=> screen.getByRole('dialog', {id:"modaleFile"}));
+      const modalBill = screen.getByRole('dialog', {id:"modaleFile"});
+      expect(modalBill).toHaveClass('show');
     })
     
     test("Then it should redirect to NewBill when we click on button New Bill", async ()=>{
@@ -55,6 +69,7 @@ describe("Given I am connected as an employee", () => {
       document.body.append(root)
       router()
       window.onNavigate(ROUTES_PATH.Bills)
+      await waitFor(() => screen.getByTestId('btn-new-bill'))
       userEvent.click(screen.getByRole('button', 'data-testid="btn-new-bill"'))
       await waitFor(() => screen.getByTestId('form-new-bill'));
       const newBillForm = screen.getByTestId('form-new-bill');
